7f2b43b906d1ecbff8af2cd5cb6c9f2f24d75136
Rohan Allison <rohan.allison@cbre.com>
2019-09-20 22:32:14 -0500

    Add a few comments and tweaks

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 )
 
 func receiveAjax(w http.ResponseWriter, r *http.Request) {
+	const userErrorMsg = "Sorry, a server error occurred"
 	
 	fmt.Println("Received ajax data.")
 
 	// Invoke ParseForm before reading form values
-	r.ParseForm()
+	err := r.ParseForm()
+	if err != nil {
+		_, _ = fmt.Fprintf(w, userErrorMsg) // give the user a generic msg in case of a hacker
+		fmt.Println(err) // log the actual error for troubleshooting
+		return
+	}
 	for key, value := range r.Form {
 		fmt.Printf("%s: %s\n", key, value)
 	}
@@ -21,17 +27,18 @@ func receiveAjax(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Message body: %s\n", r.FormValue("message"))
 
 	// Send back data to client
-	fmt.Fprintf(w, "%s: <span> %s</span><br/>", r.FormValue("username"), r.FormValue("message"))
+	_, _ = fmt.Fprintf(w, "%s: <span> %s</span><br/>", r.FormValue("username"), r.FormValue("message"))
 	
  }
 
 func main() {
+	const port = "8088"
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
     http.HandleFunc("/receive", receiveAjax)
 
 	fmt.Printf("Starting HTTP server...\n")
-    if err := http.ListenAndServe(":8088", nil); err != nil {
+    if err := http.ListenAndServe(":" + port, nil); err != nil {
         log.Fatal(err)
     }
 }
